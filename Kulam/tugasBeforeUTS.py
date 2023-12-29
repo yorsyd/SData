479026b7b5ac289c031c7af77e5e0452277d8d8c
Yora <91246717+yorsyd@users.noreply.github.com>
2023-12-29 11:38:15 +0700

    Update tugasBeforeUTS.py

--- Kulam/tugasBeforeUTS.py
+++ Kulam/tugasBeforeUTS.py
@@ -10,7 +10,24 @@ def addDataArray(arr, newData):
     newArr[-1] = newData
 
     return newArr
-    
+
+def addMidArray(arr, newData, postion):
+    # Membuat array baru dengan ukuran yang lebih besar
+    newArr = [None] * (len(arr) + 1)
+
+    # Menyalin data dari array lama ke array baru
+    for i in range(len(arr)):
+        newArr[i] = arr[i]
+
+    # Mengisi kekosongan array terakhir
+    for i in range(len(arr), postion, -1):
+        newArr[i] = arr[i-1]
+
+    # Menambahkan data baru ke pisisi tertentu
+    newArr[postion] = newData
+
+    return newArr
+
 def delDataArr(arr, arrSize, position):
     for i in range(position, arrSize-1, 1):
         arr[i] = arr[i+1]
@@ -21,9 +38,10 @@ def delDataArr(arr, arrSize, position):
     return tempArr
 
 arr = [1,2,3,4,5]
-arr = addDataArray(arr,5)
-# print(arr)
-# addDataArray(arr,2)
+arr = addMidArray(arr, 8, 2)
+print(arr)
+arr = addDataArray(arr, 5)
+print(arr)
 delDataArr(arr, 5, 2)
 print(arr)
 
